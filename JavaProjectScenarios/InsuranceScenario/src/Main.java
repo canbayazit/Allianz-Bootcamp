b1cbd193ae5f1cc6ea80ebf941e2705c3a8661e6
Can Bayazit <rovahelsing@gmail.com>
2023-08-22 23:52:40 +0300

    insurance project

---
+++ JavaProjectScenarios/InsuranceScenario/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * SENARYO:
+ *
+ * Acentamız var.
+ * Müşteri acentamıza geliyor araç sigortası yaptırmak istiyor.
+ * Müşteriye uygun sigorta teklifi oluşturuluyor
+ * Müşteri teklifi kabul ediyor ve sigorta yapılıyor.
+ *
+ * */
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
