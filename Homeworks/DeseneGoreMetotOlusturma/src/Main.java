0ddc41bbe4dfd3c39d09cb4c3143fe31b18efe75
Can Bayazit <rovahelsing@gmail.com>
2023-08-27 14:42:22 +0300

    added desene gore metot olusturma homework for patika

---
+++ Homeworks/DeseneGoreMetotOlusturma/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    static void kuralaUyanDongu(int sayi) {
+        System.out.print(sayi + " ");
+        if (sayi > 0) {
+            kuralaUyanDongu(sayi - 5);
+        }
+        System.out.print(sayi + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("N Sayısı: ");
+        int n = scanner.nextInt();
+
+        kuralaUyanDongu(n);
+    }
+}
