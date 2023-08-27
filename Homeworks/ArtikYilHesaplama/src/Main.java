04fab26803826eecf769477b2422a3b3d10935c6
Can Bayazit <rovahelsing@gmail.com>
2023-08-27 13:32:20 +0300

    added artık yıl hesaplama homework for patika

---
+++ Homeworks/ArtikYilHesaplama/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Yıl Giriniz: ");
+
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " bir artık yıldır!");
+        } else {
+            System.out.println(year + " bir artık yıl değildir!");
+        }
+        scanner.close();
+    }
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
